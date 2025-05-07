ae9c8b3554dbe0e4bdf7c5aeddfb7ab8e5a304cf
Kenil Shah <132459025+shahkenil01@users.noreply.github.com>
2025-05-07 15:45:11 +0530

    quick update

--- admin/src/utils/api.js
+++ admin/src/utils/api.js
@@ -4,7 +4,7 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const fetchDataFromApi = async (url) => {
   try {
-    const { data } = await axios.get(BASE_URL + url);
+    const { data } = await axios.get("http://localhost:4000" + url);
     return data;
   } catch (error) {
     console.log(error);
@@ -13,13 +13,13 @@ export const fetchDataFromApi = async (url) => {
 };
 
 export const postData = async (url, formData) => {
-  const { res } = await axios.post(BASE_URL + url, formData);
+  const { res } = await axios.post("http://localhost:4000" + url, formData);
   return res;
 };
 
 export const putData = async (url, formData) => {
   try {
-    const { data } = await axios.put(BASE_URL + url, formData);
+    const { data } = await axios.put("http://localhost:4000" + url, formData);
     return data;
   } catch (error) {
     console.log(error);
